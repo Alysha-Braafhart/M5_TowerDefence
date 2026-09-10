2225148d5997f5e584afb890c3c9b4aa61c43edc
Alysha-Braafhart <236565157+Alysha-Braafhart@users.noreply.github.com>
2026-09-10 12:40:42 +0200

    Slay

---
+++ Assets/Scripts/TowerPlacer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class TowerPlacer : MonoBehaviour
+{
+    public Tilemap placementMap;
+    public Tilemap nonPlaceableMap;
+
+    public GameObject ghostPrefab;
+    void Update()
+    {
+        
+    }
+}
